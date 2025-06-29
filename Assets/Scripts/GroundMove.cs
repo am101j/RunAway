280d6582c1d367966236cc0cec023a5a3e945d42
Ab <maksabeer@outlook.com>
2025-06-29 23:52:02 +0100

    Refactored movement

---
+++ Assets/Scripts/GroundMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EndlessGroundTile : MonoBehaviour
+{
+    public float speed = 10f;
+    public float tileLength = 200f;
+    public int totalTiles = 3;
+
+    void Update()
+    {
+        // Move backward
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+        // If tile went behind z = -tileLength, recycle it
+        if (transform.position.z <= -tileLength)
+        {
+            // Move it to the front by total tile space
+            transform.position += new Vector3(0, 0, tileLength * totalTiles);
+        }
+    }
+}

f8a489a1469979f9e8235b5e3ecaf02dee2ae97c
Pear277 <pearlybhalani@gmail.com>
2025-07-03 10:58:54 +0100

    added points

--- Assets/Scripts/User.cs
+++ Assets/Scripts/User.cs
@@ -5,11 +5,13 @@ public class User
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
 
-    public string FullName {get; set; }
-    public string PassHash {get; set; }
+    public string FullName { get; set; }
+    public string PassHash { get; set; }
 
-    public int LastReading {get; set; }
-    public int LastDiff {get; set;}
+    public int LastReading { get; set; }
+    public int LastDiff { get; set; }
+    
+    public int Points { get; set; } 
 
 }
 
